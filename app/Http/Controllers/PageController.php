74f4e56ac1b5340da9b8646e2342110675031e6f
michele camera <michy.cam95@gmail.com>
2022-05-23 16:50:40 +0200

    modify database

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Trains;
 use Illuminate\Http\Request;
+
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = Trains::all();
+        return view('home');
+    }
 }
---
+++ database/migrations/2022_05_23_120147_trains.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Trains extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('schedule', function (Blueprint $table) {
+            $table->id();
+            $table->string('Azienda');
+            $table->string('Stazione di partenza');
+            $table->string('Stazione di arrivo');
+            $table->time('Orario di partenza');
+            $table->time('Orario di arrivo');
+            $table->string('Codice treno');
+            $table->smallInteger('Numero carrozze');
+            $table->boolean('In orario');
+            $table->boolean('cancellato');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('schedule');
+    }
+}
